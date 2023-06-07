d662670cea1072c5414dfffd2f42b1dda87a05f7
ernest korir <ernestbore3@gmail.com>
2023-06-07 10:24:39 +0300

    Correct DOB of Blossom

--- data.sql
+++ data.sql
@@ -7,5 +7,5 @@ VALUES
 	(7,'Squirtle', TO_DATE('04/02/1993', 'MM/DD/YYYY'), 3, false, -12.13),
 	(8,'Angemon', TO_DATE('06/12/2005', 'MM/DD/YYYY'), 1, true, -45),
 	(9,'Boarmon', TO_DATE('06/07/2005', 'MM/DD/YYYY'), 7, true, 20.4),
-	(10,'Blossom', TO_DATE('10/13/2005', 'MM/DD/YYYY'), 3, true, 17),
+	(10,'Blossom', TO_DATE('10/13/1998', 'MM/DD/YYYY'), 3, true, 17),
 	(11,'Ditto', TO_DATE('05/14/2022', 'MM/DD/YYYY'), 4, true, 22);
