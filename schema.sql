7ec2b4de621836653ce906348a77c6a12f711f60
ernest korir <ernestbore3@gmail.com>
2023-06-08 20:16:56 +0300

    Specialization table

--- schema.sql
+++ schema.sql
@@ -40,3 +40,10 @@ CREATE TABLE vets (
   age INTEGER,
   date_of_graduation DATE
 );
+
+-- Create specializations table
+CREATE TABLE specializations (
+  vet_id INTEGER REFERENCES vets(id),
+  species_id INTEGER REFERENCES species(id),
+  PRIMARY KEY (vet_id, species_id)
+);
